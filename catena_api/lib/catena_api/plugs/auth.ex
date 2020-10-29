1e135d70bbce93891c3596610b880bdfe3b42f5a
Uk Chukundah <ukchukundah@gmail.com>
2020-10-29 15:17:59 +0100

    Send 403 on token expiry

--- catena_api/lib/catena_api/plugs/auth.ex
+++ catena_api/lib/catena_api/plugs/auth.ex
@@ -9,19 +9,28 @@ defmodule CatenaApi.Plug.Auth do
          {:ok, claims} <- CatenaApi.Token.verify_and_validate(token, CatenaApi.Token.signer()) do
       assign(conn, :user, %{id: claims["id"], email: claims["email"]})
     else
+      {:error, [message: "Invalid token", claim: "exp", claim_val: _]} = err ->
+        Logger.warn("Cannot access '#{path}'. Token expired. ")
+        unauthenticated(conn)
+
       [] ->
         Logger.warn("Authorization to '#{path}' failed as token was not provided")
         unauthorized(conn)
+
       err ->
         Logger.warn("Authorization to '#{path}' failed with #{inspect err}")
         unauthorized(conn)
     end
   end
 
-  defp unauthorized(conn) do
+  defp unauthorized(conn), do: error(conn, 401, "Unauthorized")
+
+  defp unauthenticated(conn), do: error(conn, 403, "Unauthenticated")
+
+  defp error(conn, status_code, msg) do
     conn
     |> put_resp_header("content-type", "application/json")
-    |> send_resp(401, Jason.encode!(%{message: "Unauthorized", success: false}))
+    |> send_resp(status_code, Jason.encode!(%{message: msg, success: false}))
     |> halt
   end
 end
