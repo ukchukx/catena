2a3dfb837cc435f0b8145200a753d08c0d99533e
Uk Chukundah <ukchukundah@gmail.com>
2020-11-16 06:46:32 +0100

    Fix typo

--- lib/catena/boundary/user_manager.ex
+++ lib/catena/boundary/user_manager.ex
@@ -117,7 +117,7 @@ defmodule Catena.Boundary.UserManager do
 
   defp schedule_next_tick do
     now = NaiveDateTime.utc_now()
-    next_new_year = NaiveDateTime.new!(now.year + 1, 1, 1, 0, 0, 0)
+    next_new_year = NaiveDateTime.new(now.year + 1, 1, 1, 0, 0, 0)
 
     Process.send_after(self(), :new_year, NaiveDateTime.diff(next_new_year, now, :millisecond))
   end
